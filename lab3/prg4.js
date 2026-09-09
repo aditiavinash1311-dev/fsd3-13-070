3f258963b3fb35f2645ebb09ffea89b19e51bc91
ADITI MISHRA <aditi.25b01011183@abes.ac.in>
2026-09-09 12:15:00 +0530

    new

--- lab3/prg4.js
+++ lab3/prg4.js
@@ -1,15 +1,26 @@
 import http from 'http'
+import { reviews , item} from './data.js';   
 
 const server = http.createServer((req,res)=>{
-if (req.url === '/api/products'){
-    res.end(JSON.stringify({
+    const products ={
     id:1,
     Name: "Mobile",
     price: 250000,
     rating: 4.5,
-    review: 123
-    }))
-} else{
+    review: 123,
+
+    };
+    
+if (req.url === '/api/products'){
+    // res.end(JSON.stringify(products));
+    res.end(JSON.stringify(item));
+    
+} else if(req.url === '/api/reviews'){
+    res.end(JSON.stringify(reviews))
+}
+
+
+else{
     res.statusCode= 404;
     res.end();
 }
