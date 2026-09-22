3e0e3767eed7ccca6643999d272038be7f888b11
ADITI MISHRA <aditi.25b01011183@abes.ac.in>
2026-09-22 14:03:56 +0530

    new

---
+++ lab3/products.js
@@ -0,0 +1,18 @@
+const products=[
+    {
+        id:1,
+        name:'marker',
+        qty:100,
+        price:15
+    },
+    {   
+        id:2,
+        name:'duster',
+        qty:200,
+        price:10
+    },
+]
+let nextId = 3;
+export const getAllProducts = () =>{
+    return products;
+}
